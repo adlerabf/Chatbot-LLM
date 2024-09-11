972ebee25fde5e3cb3e8f4bf12e50c382aaed239
adlerabf <adler.abf@gmail.com>
2024-09-10 22:27:21 -0300

    GPT-like chatbot/ with history save and load

--- llm_chains.py
+++ llm_chains.py
@@ -4,8 +4,8 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
-from langchain.vectorstores import Chroma
-import chromadb
+# from langchain.vectorstores import Chroma
+# import chromadb
 import yaml
 
 with open("config.yaml", "r") as f:
